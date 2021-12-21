47557e38dc6c836b54b5b4f060ebd55f9e20f855
KB98PL <74627004+KB98PL@users.noreply.github.com>
2021-12-21 22:42:45 +0100

    Update database.php

--- web_api/config/database.php
+++ web_api/config/database.php
@@ -1,6 +1,4 @@
 <?php
 	//phpinfo();
-	echo $_ENV["MONGODB_USER"];
-	echo 'mongodb+srv://'.$_ENV["MONGODB_USER"].':jakiesHaslo@cluster0.bf2kv.mongodb.net/wypozyczalnia';
 	$m = new MongoDB\Driver\Manager("mongodb+srv://".$_ENV['MONGODB_USER'].":".$_ENV['MONGODB_PASSWORD']."@cluster0.bf2kv.mongodb.net/wypozyczalnia");
 ?>
