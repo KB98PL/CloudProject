da19f28abc6831d107651acd7e3e5abf19512424
KB98PL <karol.blanik.1998@gmail.com>
2021-12-21 19:39:30 +0100

    zmiana nazwy

--- front_angular/karma.conf.js
+++ front_angular/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/blogkb-angular'),
+      dir: require('path').join(__dirname, './coverage/wypozyczalnia-angular'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
